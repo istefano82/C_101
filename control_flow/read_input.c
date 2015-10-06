96714e1bcf82138f17e391811f2b9cc3aa4788e2
istefano82 <ivaylo_stefanov82@yahoo.com>
2015-10-06 08:31:08 +0300

    Cleaning the directory

--- control_flow/read_input.c
+++ control_flow/read_input.c
@@ -2,14 +2,26 @@
 #include <math.h>
 int main(){
 int c;
-float root;
+scanf("%d,\n", &c);
+if (c >= 0)
+    {
+    printf("square root of %d is %4.2f\n", c , sqrt(c));
+    }
+else
+    {
+    printf("Cannot calculate square root of negative number");
+    }
+}
+
+
+/*
 while ((c = getchar()) != EOF)
     if (c >= 0)
     {
-    root = 2*c;
+    root = sqrt(c);
     printf("%s %4.2f\n", "The side lenght doubled is",  root);
     }
     else
     printf("%s\n", "Error: Negative area.");
 return 0;
-}
+}*/
